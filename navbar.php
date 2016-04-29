54da995fbf8336912b061ae400d412872afbb416
Brandon Waller <Brandon Waller>
2016-04-29 02:29:28 -0400

    a

---
+++ navbar.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="recomendationCSS.css" />
+    <title>Region Recommendation</title>
+    <link rel="stylesheet" type="text/css" href="homepage.html" />
+    <link rel="stylesheet" iype="text/css" href="regionCss.css" />
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <link href="homepagecss.css" rel="stylesheet" type="text/css" />
+    <meta charset="utf-8" />
+</head>
+<body>
+    <!---<div class="row" >
+        <object data="navbar.html" style="width:100%; height:100%;">
+
+            Your browser doesn’t support the object tag.
+        </object>
+    </div>--->
+    <div class="col-lg-4 ">
+        <ul class="nav navbar-nav">
+            <li class="dropdown col">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="menuHeader">Meals<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="Appetizers.html">Appetizers</a></li>
+                    <li><a href="Dinner.html">Dinner</a></li>
+                    <li><a href="#">Dessert</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <div class="col-lg-4 ">
+        <ul class="nav navbar-nav">
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="menuHeader">Regions<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Region 1</a></li>
+                    <li><a href="#">Region 2</a></li>
+                    <li><a href="#">Region 3</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <div class="col-lg-4 ">
+        <ul class="nav navbar-nav">
+            <li class="dropdown col">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="menuHeader">Data<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="winelist.html">View our Data</a></li>
+                    <li><a href="https://github.com/Bwall72/bwall72.github.io/raw/master/datasortedbyname.pdf">Download PDF</a></li>
+                    <li><a href="https://github.com/Bwall72/bwall72.github.io/raw/master/datasheet.xlsx">Download Excel</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+
+
+
+    <!--#region bootstrap files-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <!--#endregion-->
+</body>
+</html>
